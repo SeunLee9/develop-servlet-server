2f20b8c896d85e06bbdcbb7709185d4dfe75efe7
Seun Yi <Yiseunn@gmail.com>
2022-06-24 23:39:27 +0900

    :tada: Init : Mario begins

---
+++ Games/Mario/com/src/main/java/com/beta/mario/Game.java
@@ -0,0 +1,7 @@
+package com.beta.mario;
+
+public class Game {
+    public static final int WIDTH = 270; //초기 넓이 설정
+    public static final int HEIGHT = WIDTH; //초기 높이 설정
+    public static final int SCALE = 4;
+}
