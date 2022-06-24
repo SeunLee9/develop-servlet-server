317951c24b5ef68785d7eae6387260541634f2ac
Seun Yi <Yiseunn@gmail.com>
2022-06-24 23:49:26 +0900

    :wrench: Add : Add initial setting

--- Games/Mario/com/src/main/java/com/beta/mario/Game.java
+++ Games/Mario/com/src/main/java/com/beta/mario/Game.java
@@ -1,7 +1,16 @@
 package com.beta.mario;
 
-public class Game {
+import java.awt.*;
+
+public class Game extends Canvas{
     public static final int WIDTH = 270; //초기 넓이 설정
     public static final int HEIGHT = WIDTH; //초기 높이 설정
     public static final int SCALE = 4;
+
+    public Game() {
+        Dimension size = new Dimension(WIDTH,HEIGHT);
+        setPreferredSize(size);
+        setMaximumSize(size);
+        setMinimumSize(size);
+    }
 }
