0264dd875f8a5485aebc23bf7a806f623123faba
Seun Yi <Yiseunn@gmail.com>
2022-06-28 23:32:40 +0900

    :hammer: Add : Add development scripts

--- Games/Mario/com/src/main/java/com/beta/mario/Game.java
+++ Games/Mario/com/src/main/java/com/beta/mario/Game.java
@@ -23,7 +23,7 @@ public class Game extends Canvas{
     public synchronized void start() {
         if(running) return;
         running = true;
-        thread = new Thread();
+        thread = new Thread(); //에러뜸 왜지?? 이유를 모르겠다 this,"Thread" 적어줘야했음 지금은 에러안뜨게 한거
         thread.start();
 
     }
@@ -39,8 +39,41 @@ public class Game extends Canvas{
     }
 
     public void run() {
-
+    long lastTime = System.nanoTime();
+    long timer = System.currentTimeMillis();
+    double delta = 0.0;
+    double ns = 1000000000.0/60.0;
+    int frames = 0;
+    int ticks = 0;
+    long now = System.nanoTime();
+    System.out.println(now-lastTime);
+    while(running){
+        long now = System.nanoTime();
+        delta += (now-lastTime)/ns;
+        lastTime = new;
+        while(delta>=1){
+            tick();
+            ticks++;
+            delta--;
+        }
+        render();
+        frames++;
+        if(System.currentTimeMillis()-timer>1000){
+            timer+=1000;
+            System.out.println(frames + "Fames Per Second" + ticks + "Update")
+        }
     }
+    stop();
+    }
+
+
+
+
+
+
+
+
+
 
 
 
