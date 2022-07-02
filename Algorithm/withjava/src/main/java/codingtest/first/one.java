d394dd3a1332f5151ed5ffa1f0a1bcaa17618790
Seun Yi <Yiseunn@gmail.com>
2022-07-02 23:24:56 +0900

    :hammer: Add : Add one.one script

---
+++ Algorithm/withjava/src/main/java/codingtest/first/one.java
@@ -0,0 +1,25 @@
+package codingtest.first;
+
+import java.util.Scanner;
+
+public class one {
+    String str1 = "4242324";
+    String str2 = "Hi Im seun nice to meet you";
+
+    public static void main(String[] args) {
+//        Scanner sc = new Scanner(System.in);
+//        String str;
+//        char a, b;
+//        str = sc.nextLine();
+//        a = str.charAt(0);
+//        str = sc.nextLine();
+//        b = str.charAt(0);
+//        System.out.println(a+b);
+
+//        len(m) = 일치하는것
+//        if(len(n) 둘이상){
+//        알파벳순서에서 앞에오는걸 어떻게 입력하지}
+
+    }
+
+}
