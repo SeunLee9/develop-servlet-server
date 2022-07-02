bcd874571facc8d18b27f1e3c3bbe9ea3a95bc22
Seun Yi <Yiseunn@gmail.com>
2022-07-02 23:23:38 +0900

    :hammer: Add : Add practice code

--- Algorithm/withjava/src/main/java/practice/Practice.java
+++ Algorithm/withjava/src/main/java/practice/Practice.java
@@ -5,6 +5,10 @@ import java.io.InputStream;
 import java.util.Scanner;
 
 public class Practice {
+
+
+
+    Scanner sc = new Scanner(System.in);
     public static void main(String[] args) {
 //        InputStream in = new InputStream() {
 //            @Override
@@ -14,7 +18,11 @@ public class Practice {
 //        }
 
 //        Scanner sc = new Scanner(System.setIn("String"));// inputstream 으로 넣어야한다 여기서 바로 지정해줄수도있겠지만 난 아직 잘 모르니깐빼서 지정해줬다
-        InputStream InputStream;
-        Scanner sc = new Scanner(System.setIn(InputStream));
+//        InputStream InputStream;
+//        Scanner sc = new Scanner(System.setIn(InputStream));
+
+        Scanner sc = new Scanner(System.in);
+
+
     }
 }
