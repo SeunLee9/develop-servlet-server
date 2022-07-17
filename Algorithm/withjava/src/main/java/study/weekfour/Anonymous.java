c0a1dfda4c807c841e8e68eccfe70bcba52ee729
Seun Yi <Yiseunn@gmail.com>
2022-07-17 09:49:25 +0900

    :hammer: Add : Add study assignment code

---
+++ Algorithm/withjava/src/main/java/study/weekfour/Anonymous.java
@@ -0,0 +1,48 @@
+package study.weekfour;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Anonymous {
+
+
+
+    public static void main(String[] args) {
+        ArrayList<Car> cars = new ArrayList<>();
+        cars.add(new Car("벤츠", 2200, 0, "Black"));
+        cars.add(new Car("BMW", 2800, 2, "Green"));
+        cars.add(new Car("아우디", 2400, 3, "Blue"));
+        cars.add(new Car("폭스바겐", 1800, 1, "Pink"));
+
+        printCar(cars, new CheckCarForBigAndNotExpensive());
+    }
+
+    public static void printCar(List<Car> cars, CheckCar tester){
+        for(Car car : cars){
+            if(tester.test(car)){
+                System.out.println(car);
+            }
+        }
+    }
+
+    interface CheckCar{
+        boolean test(Car car);
+    }
+
+    // 익명클래스
+
+//    static class CheckCarForBigAndNotExpensive(){
+//        return new CheckCarForBigAndNotExpensive(){
+//
+//        }
+//    }
+    static class CheckCarForBigAndNotExpensive implements CheckCar{
+        public boolean test(Car car){
+            if (car.age >= 2 && car.price >2300){
+                System.out.println(car.name);
+            }
+            return false;
+        }
+    }
+}
