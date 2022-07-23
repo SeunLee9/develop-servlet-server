cd3d5520f089fb5af3c01924ed03d2523e60b91e
Seun Yi <Yiseunn@gmail.com>
2022-07-23 14:26:55 +0900

    :hammer: Add : Add BOJ2577 code!!

--- Algorithm/withjava/src/main/java/baek/BOJ2577.java
+++ Algorithm/withjava/src/main/java/baek/BOJ2577.java
@@ -1,7 +1,6 @@
 package baek;
 
 import java.io.*;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class BOJ2577 {
@@ -9,34 +8,73 @@ public class BOJ2577 {
         BufferedReader a = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter b = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer c = new StringTokenizer(a.readLine());
-        int d = Integer.parseInt(c.nextToken());
+        StringTokenizer d = new StringTokenizer(a.readLine());
         StringTokenizer e = new StringTokenizer(a.readLine());
-        int f = Integer.parseInt(e.nextToken());
-        StringTokenizer g = new StringTokenizer(a.readLine());
-        int h = Integer.parseInt(g.nextToken());
-        int j = d * f * h;
-        String k = Integer.toString(j); //"17037300"
-        String l[] = k.split(""); // [1, 7, 0, 3, 7, 3, 0, 0]
-//        String[] stringToSearch={};
-        int[] stringToSearch = new int[10];
-        for (int i = 0; i < 10; i++) {
-//            stringToSearch[i] = String.valueOf(i);
-            for (int z = 0; z < l.length; z++) {
-                if (String.valueOf(stringToSearch[i]+1) == l[z]) {
-                    stringToSearch[i] ++;
-                }else {
-                    continue;
-                }
+        int f = Integer.parseInt(c.nextToken());
+        int g = Integer.parseInt(d.nextToken());
+        int h = Integer.parseInt(e.nextToken());
+        int k = f*g*h;
+        String l = String.valueOf(k);
+        String[] m = l.split("");
+        int mi = 0;
+        int[] count = new int[10];
+        int total = 0;
 
+        for(int i=0;i<m.length;i++) {
+            mi = Integer.parseInt(m[i]);
+            for(int j=0;j<10;j++) {
+                if (mi == j) {
+                 count[j]++;
+                }
             }
-            b.write(String.valueOf(stringToSearch[i]));
+        }
+        for(int i =0;i< count.length;i++){
+            total = count[i];
+            b.write(String.valueOf(total));
             b.newLine();
         }
-        b.flush();
+
         b.close();
 
     }
+
 }
+
+
+
+//
+//    public static void main(String[] args) throws IOException {
+//        BufferedReader a = new BufferedReader(new InputStreamReader(System.in));
+//        BufferedWriter b = new BufferedWriter(new OutputStreamWriter(System.out));
+//        StringTokenizer c = new StringTokenizer(a.readLine());
+//        int d = Integer.parseInt(c.nextToken());
+//        StringTokenizer e = new StringTokenizer(a.readLine());
+//        int f = Integer.parseInt(e.nextToken());
+//        StringTokenizer g = new StringTokenizer(a.readLine());
+//        int h = Integer.parseInt(g.nextToken());
+//        int j = d * f * h;
+//        String k = Integer.toString(j); //"17037300"
+//        String l[] = k.split(""); // [1, 7, 0, 3, 7, 3, 0, 0]
+////        String[] stringToSearch={};
+//        int[] stringToSearch = new int[10];
+//        for (int i = 0; i < 10; i++) {
+////            stringToSearch[i] = String.valueOf(i);
+//            for (int z = 0; z < l.length; z++) {
+//                if (String.valueOf(stringToSearch[i]+1) == l[z]) {
+//                    stringToSearch[i] ++;
+//                }else {
+//                    continue;
+//                }
+//
+//            }
+//            b.write(String.valueOf(stringToSearch[i]));
+//            b.newLine();
+//        }
+//        b.flush();
+//        b.close();
+//
+//    }
+//}
 
 
 //
